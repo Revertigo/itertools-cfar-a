188b3c297d33027e73e7342481250735217b8649
Alex <44925899+alexchagan@users.noreply.github.com>
2020-06-18 12:08:03 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,344 +1,265 @@
-//
-// Created by osboxes on 6/9/20.
-//
-
 #include "doctest.h"
 #include <string>
 #include <vector>
-#include <list>
 #include <array>
+#include <list>
 #include <iostream>
-
 #include "range.hpp"
 #include "accumulate.hpp"
 #include "filterfalse.hpp"
 #include "compress.hpp"
-
+using namespace std;
 using namespace itertools;
+
+TEST_CASE("Range Test")
+{
+    vector<int> result1 {1,2,3,4,5};
+    int j=0;
+    for(int i : range(1,6))
+    {
+        CHECK(i == result1.at(j));
+        ++j;
+    }
+    CHECK(j == 5);
+    
+    vector<int> result2 {-5,-4,-3,-2,-1};
+    j=0;
+    for(int i : range(-5,0))
+    {
+        CHECK(i == result2.at(j));
+        ++j;
+    }
+    CHECK(j == 5);
+
+    
+    int ans_int=0;
+    
+    ans_int=-1;
+    for(int i: range(0,0))
+     CHECK(i == ans_int + 1); 
+    
+    
+     ans_int=0;
+    for(int i: range(1,2))
+    {
+    CHECK(i == ans_int + 1); 
+    ++ans_int;
+    }
+    
+     ans_int=0;
+    for(int i: range(1,4))
+    {
+      CHECK(i == ans_int + 1);
+      ++ans_int;
+    }
+    
+     ans_int=-2;
+    for(int i: range(-1,1))
+    {
+     CHECK(i == ans_int + 1);
+     ++ans_int;
+    }
+}
+
+TEST_CASE("Test Basic Accumulate")
+{
+    
+vector<int> vecInt {1,2,3};
+vector<int> singleVecInt {1};
+vector<string> vecString {"This", "Is", "Working"};
+vector<double> vecDouble {0.5, -2.0, 3.0};
+array<int, 3> a {-1,-2,-3};
+list<string> l {"Very" , "Good" };
+range r(1,3);
 
 int ans_int=0;
 string ans_str="";
 double ans_d=0.0;
 
-TEST_CASE("Range Tests")
-{
-    CHECK_NOTHROW(range(0,0)); //1
-    ans_int=-1;
-    for(int i: range(0,0))
-                CHECK(i == ans_int + 1); //2
-
-    ans_int=0;
-    for(int i: range(1,2))
-                CHECK(i == ans_int + 1); //3
-
-    ans_int=0;
-    for(int i: range(1,4))
-                CHECK(i == ans_int + 1); //4
-
-            CHECK_NOTHROW(range(-1,1)); //5
-    ans_int=-2;
-    for(int i: range(-1,1))
-                CHECK(i == ans_int + 1);
-
-    CHECK_THROWS(range(2,1)); //7
-    CHECK_THROWS(range(-1,-3)); //49
-    CHECK_THROWS(range('a','c')); //48
-    CHECK_THROWS(range(0.5,1)); //50
-
-    int follow = 100;
-    for(int i: range(100, 115))
-    {
-        CHECK(follow++ == i);
-    }
-}
-
-vector<int> vecInt = {1,2,3};
-vector<int> singleVecInt {1};
-vector<int> emptyVecInt {};
-vector<string> vecString = {"This", "Is", "Working"};
-vector<double> vecDouble = {0.5, -2.0, 3.0};
-array<int, 3> a = {-1,-2,-3};
-vector<string> l = {"Very" , "Good" };
-range r(1,3);
-
-
-TEST_CASE("Basic Accumulate Tests")
-{
-    CHECK_NOTHROW(for(auto i: accumulate(vecInt)));
-    CHECK_NOTHROW(for(auto i: accumulate(singleVecInt))); //9
-    CHECK_THROWS(for(auto i: accumulate(emptyVecInt))); //10
-    CHECK_NOTHROW(for(auto i: accumulate(vecString))); //11
-    CHECK_NOTHROW(for(auto i: accumulate(vecDouble))); //12
-    CHECK_NOTHROW(for(auto i: accumulate(a))); //13
-    CHECK_NOTHROW(for(auto i: accumulate(l))); //14
-    CHECK_NOTHROW(for(auto i: accumulate(r))); //15
-    CHECK_NOTHROW(accumulate(accumulate(vecInt))); //22
-
     ans_int=0;
     for(int i: accumulate(vecInt))
-        ans_int+=i;
-            CHECK(ans_int == 10); //16
-
+    ans_int+=i;
+    CHECK(ans_int == 10); 
+    
     ans_int=0;
     for(int i: accumulate(singleVecInt))
-        ans_int+=i;
-            CHECK(ans_int == 1); //17
-
-    ans_str = "";
-    for(string i: accumulate(vecString))
-        ans_str+=i;
-            CHECK(ans_str == "ThisThisIsThisIsWorking"); //18
-
-    ans_d = 0.0;
-    for(double i: accumulate(vecDouble))
-        ans_d+=i;
-            CHECK(ans_d == 2.5); //19
-
-    ans_int=0;
-    for(int i:accumulate(a))
-        ans_int+=i;
-            CHECK(ans_int == -10); //20
-
-    ans_str = "";
-    for(string i: accumulate(l))
-        ans_str+=i;
-            CHECK(ans_str == "VeryVeryGood"); //21
-
-    ans_int=0;
+    ans_int+=i;
+    CHECK(ans_int == 1); 
+    
+     ans_d = 0.0;
+     for(double i: accumulate(vecDouble))
+     ans_d+=i;
+     CHECK(ans_d == 0.5); 
+     
+      ans_int=0;
+     for(int i:accumulate(a))
+     ans_int+=i;
+     CHECK(ans_int == -10); 
+     
+      ans_int=0;
     for(int i: accumulate(r))
-        ans_int+=i;
-            CHECK(ans_int == 4); //23
+     ans_int+=i;
+     CHECK(ans_int == 4); 
+     
+       ans_int=0;
+     for(int i: accumulate(accumulate(vecInt)))
+     ans_int+=i;
+     CHECK(ans_int == 37); 
+     
+       ans_int=0;
+     for(int i: accumulate(accumulate(accumulate(vecInt))))
+     ans_int+=i;
+     CHECK(ans_int == 64); 
+     
+    vecInt = {1,2,3};
+    vector<int> result1 {1,3,6};
+    int j=0;
+    for(int i : accumulate(vecInt))
+    {
+        CHECK(i == result1.at(j));
+        ++j;
+    }
+    CHECK(j == 3);
 
-    ans_int=0;
-    for(int i: accumulate(accumulate(vecInt)))
-        ans_int+=i;
-            CHECK(ans_int == 10); // 24
+    
+    
+    vector<int> result2 {1};
+    j=0;
+    for(int i : accumulate(singleVecInt))
+    {
+        CHECK(i == result2.at(j));
+        ++j;
+    }
+    CHECK(j == 1);
 
-    ans_int=0;
-    for(int i: accumulate(accumulate(accumulate(vecInt))))
-        ans_int+=i;
-            CHECK(ans_int == 15); // 25
+    
+    vector<string> result3 {"This","ThisIs","ThisIsWorking"};
+    j=0;
+    for(auto i : accumulate(vecString))
+    {
+        CHECK(i == result3.at(j));
+        ++j;
+    }
+    CHECK(j == 3);
+
+    
+    vector<double> result4 {0.5,1.0,1.5};
+    j=0;
+    for(double i : accumulate(vector<double>{0.5,0.5,0.5}))
+    {
+        CHECK(i == result4.at(j));
+        ++j;
+    }
+    CHECK(j == 3);
 
 }
 
-TEST_CASE("Accumulate With Binary Operator")
+TEST_CASE("Accumulate with binary operator")
 {
-    CHECK_NOTHROW(accumulate(vecInt,[](int x, int y){return x+y;})); //26
-    CHECK_NOTHROW(accumulate(singleVecInt,[](int x, int y){return x*y;})); //27
-    CHECK_THROWS(accumulate(emptyVecInt,[](int x, int y){return x+y;})); //28
-    CHECK_NOTHROW(accumulate(vecString,[](int x, int y){return x+y;}));//29
-    CHECK_THROWS(accumulate(vecString,[](int x, int y){return x*y;}));//30
-    CHECK_NOTHROW(accumulate(vecDouble,[](int x, int y){return x/y;})); //31
-    CHECK_NOTHROW(accumulate(a,[](int x, int y){return x-y;})); //32
-    CHECK_NOTHROW(accumulate(l,[](int x, int y){return x*y;})); //33
-    CHECK_NOTHROW(accumulate(r,[](int x, int y){return x+y;})); //34
-    CHECK_NOTHROW(accumulate(accumulate(vecInt),[](int x, int y){return x+y;})); //35
-    CHECK_THROWS(accumulate(accumulate(vecInt),"test")); //97
 
-    ans_int=0;
-    for(int i: accumulate(vecInt,[](int x, int y){return x+y;}))
-        ans_int+=i;
-            CHECK(ans_int == 10); //36
+    vector<int> result1 {1,3,6,10,15,21,28,36,45,55};
+    int j=0;
+    for(int i : accumulate(range(1,11),[](int x, int y){return x+y;}))
+    {
+        CHECK(i == result1.at(j));
+        ++j;
+    }
+    CHECK(j == 10);
 
-    ans_int=0;
-    for(int i: accumulate(vecInt,[](int x, int y){return x*y;}))
-        ans_int+=i;
-            CHECK(ans_int == 9); //51
+   
 
-    ans_int=0;
-    for(int i: accumulate(vecInt,[](int x, int y){return x-y;}))
-        ans_int+=i;
-            CHECK(ans_int == -4); //52
-
-    ans_int=0;
-    for(int i: accumulate(singleVecInt,[](int x, int y){return x*y;}))
-        ans_int+=i;
-            CHECK(ans_int == 1); //37
-
-    ans_str = "";
-    for(string i: accumulate(vecString,[](string x, string y){return x+y;}))
-        ans_str+=i;
-            CHECK(ans_str == "ThisThisIsThisIsWorking"); //38
-
-    ans_d = 0.0;
-    for(double i: accumulate(vecDouble,[](int x, int y){return x/y;}))
-        ans_d+=i;
-            CHECK(ans_d == 0.2); //39
-
-    ans_int=0;
-    for(int i:accumulate(a,[](int x, int y){return x-y;}))
-        ans_int+=i;
-            CHECK(ans_int == 4); //40
-
-    ans_str = "";
-    for(string i: accumulate(l,[](string x, string y){return x+y;}))
-        ans_str+=i;
-            CHECK(ans_str == "VeryVeryGood"); //41
-
-    ans_int=0;
-    for(int i: accumulate(r,[](int x, int y){return x+y;}))
-        ans_int+=i;
-            CHECK(ans_int == 4); //42
-
-    ans_int=0;
-    for(int i: accumulate(accumulate(vecInt ,[](int x, int y){return x+y;}),[](int x, int y){return x+y;}))
-        ans_int+=i;
-            CHECK(ans_int == 15); // 43
-
-    ans_int=0;
-    for(int i: accumulate(accumulate(accumulate(vecInt,[](int x, int y){return x+y;}),[](int x, int y){return x+y;}),[](int x, int y){return x+y;}))
-        ans_int+=i;
-            CHECK(ans_int == 21); // 44
-
-            CHECK_THROWS(accumulate(vecString,[](int x, int y){return x/y;})); //45
-            CHECK_THROWS(accumulate(vecString,[](int x, int y){return x%y;})); //46
-            CHECK_THROWS(accumulate(vecString,[](int x, int y){return x-y;})); //47
+vector<int> result2 {1,2,6};
+     j=0;
+    for(int i : accumulate(vector<int>{1,2,3} ,[](int x, int y){return x*y;}))
+    {
+        CHECK(i == result2.at(j));
+        ++j;
+    }
+    CHECK(j == 3);
 
 }
 
-struct lessThan3{
-    bool operator()(int i) const {return i<3;}
-};
+TEST_CASE("Filter False")
+{
+    vector<int> result1 {2,4,6,8,10,12,14,16};
+    int j=0;
+    for(int i : filterfalse([](int i){return i%2!=0;},range(2,17)))
+    {
+        CHECK(i == result1.at(j));
+        ++j;
+    }
+    CHECK(j == 8);
 
-struct moreThan2{
-    bool operator()(int i) const {return i>2;}
-};
+    
+    vector<int> result2 {3,5,7,9,11,13,15};
+    j=0;
+    for(int i : filterfalse([](int i){return i%2==0;},range(2,17)))
+    {
+        CHECK(i == result2.at(j));
+        ++j;
+    }
+    CHECK(j == 7);
 
-struct equal2{
-    bool operator()(int i) const {return i==2;}
-};
+   
+    vector<int> result3 {3,6,10,15,21,28,36,45,55};
+    vector<int> vec{1,2,3,4,5,6,7,8,9,10};
+    j=0;
+    for(int i : filterfalse([](int i){return i<2;},accumulate(vec)))
+    {
+        CHECK(i == result3.at(j));
+        ++j;
+    }
+    CHECK(j == 9);
 
-
-TEST_CASE("Filter False") {
-            CHECK_NOTHROW(filterfalse(lessThan3{}, vecInt)); //53
-            CHECK_NOTHROW(filterfalse(moreThan2{}, vecInt)); //54
-            CHECK_NOTHROW(filterfalse(equal2{}, vecInt)); //55
-            CHECK_NOTHROW(filterfalse([](int i) { return i % 2 == 0; }, r)); //56
-            CHECK_NOTHROW(filterfalse([](int i) { return i + i == 2; }, r)); //57
-            //CHECK_NOTHROW(filterfalse([](string i) { return i + "Good" == "VeryGood"; }, l)); //58
-            CHECK_NOTHROW(filterfalse(moreThan2{}, accumulate(r))); //59
-            CHECK_NOTHROW(filterfalse(moreThan2{}, accumulate(vecInt, [](int x, int y) { return x * y; }))); //60
-            CHECK_THROWS(filterfalse([](double i) { return i / 2.0 == 0.0; }, accumulate(vecDouble))); //61
-            CHECK_NOTHROW(filterfalse([](string i) { return i.size() == 4; }, l)); //62
-
-            CHECK_THROWS(filterfalse([](int i) { return i / 2 == 0.5; }, vecInt)); //63
-            //CHECK_THROWS(filterfalse([](int i) { return i.size() == 4; }, vecString)); //64
-            CHECK_THROWS(filterfalse([](int i) { return i % 2 == 0; }, vecString)); //65
-            CHECK_THROWS(filterfalse([](int i) { return i % 2 == 0; }, emptyVecInt)); //66
-
-    ans_str = "";
-    for (int i: filterfalse(lessThan3{}, vecInt))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "1 2 "); //67
-
-    ans_str = "";
-    for (int i: filterfalse(moreThan2{}, vecInt))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "3 "); //68
-
-    ans_str = "";
-    for (int i: filterfalse(equal2{}, vecInt))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "2 "); //69
-
-    ans_str = "";
-    for (int i: filterfalse([](int i) { return i % 2 == 0; }, r))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "2 ");
-
-    ans_str = "";
-    for (int i: filterfalse([](int i) { return i + i == 2; }, r))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "1 "); //70
-
-//    ans_str = "";
-//    for (auto i: filterfalse([](string i) { return i + "Good" == "VeryGood"; }, l))
-//        ans_str = ans_str + to_string(i) + " ";
-//            CHECK(ans_str == "Very "); //71
-
-    ans_str = "";
-    for (int i: filterfalse(moreThan2{}, accumulate(r)))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "3 "); //72
-
-    ans_str = "";
-    for (int i: filterfalse(moreThan2{}, accumulate(vecInt, [](int x, int y) { return x * y; })))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "6 "); //73
-
-    ans_str = "";
-    for (double i: filterfalse([](double i) { return i / 2.0 == 0.0; }, accumulate(vecDouble)))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == ""); //74
-
-//    ans_str = "";
-//    for (int i: filterfalse([](string i) { return i.size() == 4; }, l))
-//        ans_str = ans_str + to_string(i) + " ";
-//            CHECK(ans_str == "Very "); //75
 }
 
-TEST_CASE("Compress"){
+TEST_CASE("COMPRESS")
+{
+ 
+ vector<bool> tff{true,false,false};
+ vector<bool> fff{false,false,false};
+ vector<bool> ttt{true,true,true};
+ vector<bool> tft{true,false,true};
+ vector<bool> fft{false,false,true};
+ vector<bool> tfft{true,false,false,true};
+  vector<bool> ttf{true,true,false};
+ vector<bool> ttfttttftt{true,true,false,true,true,true,true,false,true,true};
+  CHECK_NOTHROW(compress(string("abc"), tff)); //76
+  CHECK_NOTHROW(compress(string("abc"), fff)); //77
+  CHECK_NOTHROW(compress(string("abc"), ttt)); //78
+  CHECK_NOTHROW(compress(string("abc"), tft)); //79
+  CHECK_NOTHROW(compress(string("abc"), fft)); //80
+  
+    vector<int> result1{2,4,6};
+    int j=0;
+    for(auto i : compress(vector<int>{2,4,6},ttt))
+    {
+        CHECK(i == result1.at(j));
+        ++j;
+    }
+    CHECK(j == 3);
 
-    vector<bool> tf = {true, false};
-    vector<bool> tff = {true, false, false};
-    vector<bool> fff = {false, false, false};
-    vector<bool> ttt = {true, true, true};
-    vector<bool> tft = {true, false, true};
-    vector<bool> fft = {false, false, true};
-    vector<bool> ftf = {false, true, false};
-    vector<bool> tfft = {true, false, false ,true};
+    
+    vector<string> result2 = {"Hi","Bye"};
+    j=0;
+        for(auto i : compress(vector<string>{"Hi","Hello","Bye"},tft))
+    {
+        CHECK(i == result2.at(j));
+        ++j;
+    }
+    CHECK(j == 2);
 
-    CHECK_NOTHROW(compress(string("abc"), tff)); //76
-    CHECK_NOTHROW(compress(string("abc"), fff)); //77
-    CHECK_NOTHROW(compress(string("abc"), ttt)); //78
-    CHECK_NOTHROW(compress(string("abc"), tft)); //79
-    CHECK_NOTHROW(compress(string("abc"), fft)); //80
-   //CHECK_NOTHROW(compress(vecInt, ttt)); //81
-    CHECK_NOTHROW(compress(range(1,4), tff)); //82
-    CHECK_NOTHROW(compress(accumulate(vecInt), tff)); //83
-    CHECK_NOTHROW(compress( accumulate(vecInt,[](int x, int y){return x*y;}), fft) ); //84
-    CHECK_NOTHROW(compress( filterfalse([](int i){return i%2==0;},accumulate(vecInt,[](int x, int y){return x*y;})), fft)); //85
-    CHECK_THROWS(compress(string("abc"), tfft)); //86
-    CHECK_THROWS(compress(string("abc"), tft)); //87
-    CHECK_THROWS(compress(string("abcd"), tff)); //88
-    CHECK_THROWS(compress(string("abc"), fft)); //87
-//    CHECK_THROWS(compress()); //99
-    CHECK_THROWS(compress(string("gh"), tf)); //100
-
-    ans_str ="";
-    for(auto i: compress(  string("abc"), tff  ))
-        ans_str = ans_str + i + " ";
-            CHECK(ans_str == "a "); //88
-
-    ans_str ="";
-    for(auto i: compress(  string("abc"), fff  ))
-        ans_str = ans_str + i + " ";
-            CHECK(ans_str == ""); //89
-
-    ans_str ="";
-    for(auto i: compress(  string("abc"), ttt  ))
-        ans_str = ans_str + i + " ";
-            CHECK(ans_str == "a b c "); //90
-
-    ans_str ="";
-    for(auto i: compress(  string("abc"), tft  ))
-        ans_str = ans_str + i + " ";
-            CHECK(ans_str == "a c "); //91
-
-//    ans_str ="";
-//    for(int i: compress(  vecInt, ttt  ))
-//        ans_str = ans_str + to_string(i) + " ";
-//            CHECK(ans_str == "1 2 3 "); //92
-
-    ans_str ="";
-    for(int i: compress(  range(1,4), tff  ))
-        ans_str = ans_str + to_string(i) + " ";
-            CHECK(ans_str == "1 "); //93
-
-    ans_str ="";
-    for(int i: compress(  accumulate(vecInt), ftf  ))
-        ans_str = ans_str + to_string(i) + " ";
-    CHECK(ans_str == "3 "); //94
+   
+    vector<int> result3 = {1,2,4,5,6,7,9,10};
+    j=0;
+    for(int i : compress(range(1,11),ttfttttftt))
+    {
+        CHECK(i == result3.at(j));
+        ++j;
+    }
+    CHECK(j == 8);
+    ///////////////////
+    
 }
 
-
+    
